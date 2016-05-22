ef7a2220c518330b50b2c1ac6306a56fc41e7a4c
Jamie Farrelly <jamie.farrelly@paywithfire.com>
2016-05-22 14:56:42 +0100

    Extension complete(old extension I made)

---
+++ background.js
@@ -0,0 +1,20 @@
+var currentUrl = document.URL;
+
+onload = function() {
+
+    //if we're on irishtimes.com, do some work ;)
+    if (currentUrl.indexOf("irishtimes.") !== -1) {
+
+        javascript: (function() {
+            C = document.cookie.split("; ");
+            for (d = "." + location.host; d; d = ("" + d).substr(1).match(/\..*$/))
+                for (sl = 0; sl < 2; ++sl)
+                    for (p = "/" + location.pathname; p; p = p.substring(0, p.lastIndexOf('/')))
+                        for (i in C)
+                            if (c = C[i]) {
+                                document.cookie = c + "; domain=" + d.slice(sl) + "; path=" + p.slice(1) + "/" + "; expires=" + new Date((new Date).getTime() - 1e11).toGMTString()
+                            }
+        })();
+
+    }
+}
